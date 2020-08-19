a731de0c0d87b6e54b441cc689ebfe0755438aa6
bora.karaca <bora.karaca@tekhnelogos.com>
2020-08-19 15:21:11 +0300

    first commit

---
+++ CoreConsoleBasicExample/CoreConsoleBasicExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoreConsoleBasicExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
